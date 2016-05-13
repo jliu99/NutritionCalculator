c7bea6123b9a6f7eeaaf6e13c493f3af97ed88ab
jl <jl@10.150.99.127>
2016-05-13 14:05:30 -0600

    Made a rudimentary runner/UI to be edited later

--- src/nutritioncalculator/NutritionCalculator.java
+++ src/nutritioncalculator/NutritionCalculator.java
@@ -5,6 +5,8 @@
  */
 package nutritioncalculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kslimmo
@@ -15,7 +17,19 @@ public class NutritionCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String command = "";
+        System.out.println("Welcome! This is a WIP Nutrition Calculator.");
+        System.out.println("Type: create, quit");
+        while(!command.equals("quit")){
+        command = input.nextLine();
+        command = command.toLowerCase();
+            if(command.equals("create")){
+                //UserLog ul = new UserLog();
+                System.out.println("Created new log");
+            } else if (! command.equals("quit")){
+                System.out.println("Sorry, I did not understand the command: " + command);
+            }
+        }
     }
-    
 }
