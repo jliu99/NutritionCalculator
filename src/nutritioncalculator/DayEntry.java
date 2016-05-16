8e1197e227d4f3a0fac94237d6cc6e2082b64ff7
jl <jl@10.150.99.127>
2016-05-16 14:55:59 -0600

    Created DayEntry and MealEntry classes.

---
+++ src/nutritioncalculator/DayEntry.java
@@ -0,0 +1,13 @@
+package nutritioncalculator;
+
+import java.util.*;
+import nutritioncalculator.MealEntry;
+
+public class DayEntry{
+    private MealEntry[] meals;
+    
+    public NutritionFacts getNutritionValue(){
+        //For Kelly
+        return null;
+    }
+}
---
+++ src/nutritioncalculator/MealEntry.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nutritioncalculator;
+
+import java.util.*;
+/**
+ *
+ * @author jl
+ */
+public class MealEntry {
+
+        private ArrayList<String> contents;
+        private NutritionFacts nf;
+
+}
